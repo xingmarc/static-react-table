65ab53b67b58e48c46969c8f03c3495a56058c6d
xingmarc <yichixing@gmail.com>
2017-03-21 15:24:56 -0700

    Change index.js structure.

---
+++ index.js
@@ -0,0 +1,7 @@
+var Table = require('./dist/table.boundle.js')
+var Column = require('./dist/column.boundle.js')
+
+module.export = {
+	Table: Table,
+	Column: Column
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
+    entry: {
+        table: path.join(__dirname, "src", "Table.js"),
+        column: path.join(__dirname, "src", "Column.js")
+    },
     output: {
-        path: path.join(__dirname),
-        filename: "index.bundle.js"
+        path: path.join(__dirname, "dist"),
+        filename: "[name].bundle.js"
     },
 
     module: {
